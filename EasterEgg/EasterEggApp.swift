dbd5ea9813e97beb772020d2e5828164ba8f3f68
kevin marinho <kevindmarinho4321@gmail.com>
2023-01-31 15:23:20 -0400

    Initial Commit

---
+++ EasterEgg/EasterEggApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasterEggApp.swift
+//  EasterEgg
+//
+//  Created by kevin marinho on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct EasterEggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
